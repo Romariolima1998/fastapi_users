913df67666256e5bd8f3fd1d3751bd9af09b65a0
Romario lima <romariolima1006@gmail.com>
2024-10-08 14:52:58 -0300

    def user_list: view para listar usuario

--- users/routers/users.py
+++ users/routers/users.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.future import select
 
 from users.database.database import get_session
 from users.database.models import User
@@ -25,8 +26,17 @@ async def user_create(dados: UserCreateInput, session: Session):
         session.add(user)
         await session.commit()
         await session.refresh(user)
-        
+
         return user
     
     except SQLAlchemyError as error:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail='error saving to database')
+    
+
+@router.get('/list')
+async def user_list(session: Session, offset: int = 0, limit: int = 10) -> list[UserCreateOutput]:
+    users = await session.scalars(
+        select(User).offset(offset).limit(limit)
+    )
+
+    return users.all()
